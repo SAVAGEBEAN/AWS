23b80eb8b84acef87bee3107e54a2fc5bb9fe9de
SAVAGEBEAN <vijesh.chaudhari19@comp.sce.edu.in>
2022-03-07 13:02:46 +0530

    sec commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/Home', (req,res)=>{
-    res.json("Welcome to hoe page")
+    res.send("Welcome to hoe page")
 })
 app.use(express.json())
-app.listen(9000, ()=>{
+app.listen(5000, ()=>{
     console.log(`server is running`)
 })
