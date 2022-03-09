5d28ab418aa0027eeddb8ca0c973321327cc352a
SAVAGEBEAN <88079234+SAVAGEBEAN@users.noreply.github.com>
2022-03-09 10:33:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req,res)=>{
     res.send("Welcome to hoe page")
 })
 app.use(express.json())
-app.listen(5000, ()=>{
-    console.log(`server is running`)
-})
+app.listen(process.env.PORT || 5000)
+
+module.exports = app
