6c34a151bb43e0ac634c7728ef3a72d2406c5161
SAVAGEBEAN <88079234+SAVAGEBEAN@users.noreply.github.com>
2022-03-09 11:04:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send("Welcome to hoe page!.. Home*")
+    res.send("Welcome to hoe page. Home*")
     res.send("Welcum")
 })
 app.use(express.json())
