7aeea014dd37da73fef5baed9cc0635f28787c82
SAVAGEBEAN <88079234+SAVAGEBEAN@users.noreply.github.com>
2022-03-09 10:55:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send("Welcome to hoe page")
+    res.send("Welcome to hoe page!.. Home*")
 })
 app.use(express.json())
 app.listen(process.env.PORT || 5000)
